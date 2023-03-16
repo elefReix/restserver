f2403e862fd8763f78594ade7650b29fefd345c8
eco.recycling.desarrollo@gmail.com <eco.recycling.desarrollo@gmail.com>
2023-03-16 14:06:59 -0700

    clase : handlebars

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const hbs = require('hbs')
+
+const port = process.env.PORT
+
+hbs.registerPartials(__dirname + '/views/partials',(err)=>{})
+app.set('view engine','hbs')
 
 //configuracion middleware
 app.use( express.static('public') )
 
 app.get('/ping', (req,res)=>{
-    res.send('hola elef')
+    res.send('hola elef')  
 })
 
+app.get('/', (req,res)=>{
+    res.render('home',{
+        nombre :' Katiana Vazquez Corrales',
+        titulo:'born'
+    })
+})
+
+
+
 app.get('/generic', (req,res)=>{
-    res.sendFile(__dirname+'/public/generic.html')
+    res.render('generic')
+    // res.sendFile(__dirname+'/public/generic.html')
 })
 
 app.get('/elements', (req,res)=>{
-    res.sendFile(__dirname+'/public/elements.html')
+    res.render('elements')
 })
 
 app.get('/index', (req,res)=>{
@@ -24,4 +41,4 @@ app.get('*', (req,res)=>{
     res.sendFile(__dirname+'/public/404.html')
 })
 
-app.listen( 3017, ()=> {console.log('Server Corriendo en el puerto 3017')} )
+app.listen( port, ()=> {console.log(`Server Corriendo en el puerto ${port}`)} )
