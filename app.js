2b67b4a3fcb49eab8def7dc35289780dfbaaefd9
eco.recycling.desarrollo@gmail.com <eco.recycling.desarrollo@gmail.com>
2023-04-13 09:47:22 -0700

    update : esqueleto server rest

--- app.js
+++ app.js
@@ -1,44 +1,6 @@
 require('dotenv').config()
-const express = require('express')
-const app = express()
-const hbs = require('hbs')
 
-const port = process.env.PORT
+const Server = require('./models/server')
 
-hbs.registerPartials(__dirname + '/views/partials',(err)=>{})
-app.set('view engine','hbs')
-
-//configuracion middleware
-app.use( express.static('public') )
-
-app.get('/ping', (req,res)=>{
-    res.send('hola elef')  
-})
-
-app.get('/', (req,res)=>{
-    res.render('home',{
-        nombre :' Katiana Vazquez Corrales',
-        titulo:'born'
-    })
-})
-
-
-
-app.get('/generic', (req,res)=>{
-    res.render('generic')
-    // res.sendFile(__dirname+'/public/generic.html')
-})
-
-app.get('/elements', (req,res)=>{
-    res.render('elements')
-})
-
-app.get('/index', (req,res)=>{
-    res.sendFile(__dirname+'/public/index.html')
-})
-
-app.get('*', (req,res)=>{
-    res.sendFile(__dirname+'/public/404.html')
-})
-
-app.listen( port, ()=> {console.log(`Server Corriendo en el puerto ${port}`)} )
+const server = new Server()
+server.listen()
---
+++ models/server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const cors = require('cors')
+class Server {
+    
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT
+        this.usuariosPath = '/api/usuarios'
+        //Middlewares
+        this.middleware()
+        //Rutas 
+        this.routes()
+    }
+
+    middleware(){
+        //CORS
+        this.app.use(cors())
+
+        //Informacion que venga en post, put o delete lo serializa a un objecto json
+        this.app.use(express.json())
+
+        //Directorio publico
+        this.app.use(express.static('public'))
+    }
+
+    routes(){
+       this.app.use(this.usuariosPath,require('../routes/usuarios'))
+    }
+
+    listen(){
+        this.app.listen( this.port, ()=> {console.log(`Server Corriendo en el puerto ${this.port}`)} )
+    }
+
+}
+
+module.exports = Server
